997fa034e54bada38e2bd04416024b1bd45da9bc
harristheharris <anthonyharris7777@gmail.com>
2024-02-25 00:46:08 -0500

    added local storage and added color to each div depenedent on the time

--- Assets/script.js
+++ Assets/script.js
@@ -3,55 +3,110 @@
 // in the html.
 
 //we save whatever they input and the location they input it in aka the time block
-function saveItem(task , key) {
+function saveItem(taskAndkey) {
   console.log("This is blue devil! Do you copy HQ?! HQ?! We're in the saveItem() function and need orders!!!");
- 
-  console.log("adding this task: " + task + " This task is associated with this time: " + key);
-  
-  
-  //var taskAndtime = {theTask: task , theTime: key};
-  var taskAndtime = {};
-  taskAndtime.theTask = task;
-  taskAndtime.theTime = key;
-  console.log(taskAndtime.theTime);
-  console.log(taskAndtime.theTask);
-  console.log(taskAndtime);
 
-  var savedTasks = JSON.parse(localStorage.getItem(key));
+  console.log(taskAndkey);
+  var savedTasks = JSON.parse(localStorage.getItem(taskAndkey.theTime));
 
-  savedTasks.push(taskAndtime);
+  savedTasks = savedTasks || []; //this is so cool. We asign savedTasks to itself or if savedTasks is empty/null it will asign savedTasks to an empty array. Big fan of this. Quick and short 
+  savedTasks.push(taskAndkey);
 
-  localStorage.setItem(key, JSON.stringify(savedTasks));
-  
+  localStorage.setItem(taskAndkey.theTime, JSON.stringify(savedTasks));
 
 }
 
+function validateData(isTaskempty) {
+
+  console.log("inside validateData function");
+  console.log(isTaskempty);
+
+  if (isTaskempty.theTask === ' ') {
+    let truthValue = false;
+    console.log("Task input field empty");
+    return truthValue;
+  }
+
+  else {
+    let truthValue = true;
+    console.log("Task input field filled");
+    saveItem(isTaskempty);
+    return truthValue;
+
+  }
+
+}
+
+function whatTimeisIt() {
+
+  let currentHour = dayjs().get('hour');
+  //let currentHour = 10;
+  let currentMinute = dayjs().get('minute');
+  console.log(currentHour);
+  console.log(currentMinute);
+
+
+  $(".container-fluid").children().each(function () {
+
+    let currentDiv = $(".container-fluid").children();
+
+    let timeTocompare = $(this).data("time");
+    console.log(timeTocompare);
+
+    if (timeTocompare == currentHour) {
+
+      console.log("red");
+      console.log(this);
+      $(this).addClass("present")
+
+    }
+
+    if (timeTocompare < currentHour) {
+
+      console.log("white");
+      console.log(this);
+      $(this).addClass("past")
+
+    }
+
+    if (timeTocompare > currentHour) {
+
+      console.log("green");
+      console.log(this);
+      $(this).addClass("future")
+    }
+  });
+
+}
+
+
 $(function () {
 
-  var newTask = {};
+  whatTimeisIt();
 
-  $(".saveBtn").on("click" , function (){
-  console.log($(this).parent().attr("id"));
 
-      let taskInput = $(this).siblings("textarea").val();
-      console.log(taskInput);
-      let keyTotheCastle = $(this).parent().attr("id");
-      let savedTask = taskInput;
-      console.log(savedTask);
 
-    saveItem(taskInput , keyTotheCastle);
+  //we are pointing at the button using the 'this' keyword. Once we point at the button we specify further with the parent() method. This specifies that we are looking at the parent element  of the 'this' aka the button. We specify further with the attr() which, in this case, grabs the id of that parent element
+  $(".saveBtn").on("click", function () {
+    console.log($(this).parent().attr("id"));
+
+
+    let taskInput = $(this).siblings("textarea").val();
+
+    console.log(taskInput);
+    let keyTotheCastle = $(this).parent().attr("id");
+
+    //var taskAndtime = {theTask: task , theTime: key};
+    var taskAndtime = {};
+    taskAndtime.theTask = taskInput;
+    taskAndtime.theTime = keyTotheCastle;
+    console.log(taskAndtime.theTime);
+    console.log(taskAndtime.theTask);
+    console.log(taskAndtime);
+
+    validateData(taskAndtime); //this validate that the taskInput is not empty. It still fills the array with a blank taskInput
+
   });
- //we are pointing at the button using the 'this' keyword. Once we point at the button we specify further with the parent() method. This specifies that we are looking at the parent element  of the 'this' aka the button. We specify further with the attr() which, in this case, grabs the id of that parent element
-
-
-
-  
-
-  // TODO: Add a listener for click events on the save button.
-  // This code should use the id in the containing time-block as a key to save the user input in local storage. 
-  // HINT: What does `this` reference in the click listener function?
-  // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
-
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
