6f3142abb0a3812ce9d1d6bb1193d21ad527ed0c
harristheharris <anthonyharris7777@gmail.com>
2024-02-25 07:19:17 -0500

    add the date displayed

--- Assets/script.js
+++ Assets/script.js
@@ -112,12 +112,26 @@ function renderData(key) {
  //localStorage.clear();
 }
 
+function displayDate(){
+ 
+  let aYear = dayjs().get('year');
+  let aMonth = dayjs().get('month');
+  let aDate = dayjs().get('date');
+  console.log(aMonth);
+  let proxyDate = aMonth + 1 + "-" + aDate + "-" + aYear; //why in the hell does month starts at 0 lol
+  
+  let currentDate = dayjs(proxyDate).format('MM/DD/YYYY');
+  console.log(currentDate);
+  $("#currentDay").text(currentDate);
+
+}
+
 
 //-------------------------------------------------------------------------------------------------------------------------------
 
 $(function () {
 
-
+  displayDate();
   whatTimeisIt();
   
 
@@ -159,8 +173,4 @@ $(function () {
   });
 
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. 
-  // HINT: How can the id attribute of each time-block be used to do this?
-  // TODO: Add code to display the current date in the header of the page.
 });
