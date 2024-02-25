cecc0965ae5fa091d807c6bfbabea519f64c44b5
harristheharris <anthonyharris7777@gmail.com>
2024-02-25 06:43:02 -0500

    we have saved tasks, times being a certain coler depending on the current time, and task being display on respective divs

--- Assets/script.js
+++ Assets/script.js
@@ -3,55 +3,57 @@
 // in the html.
 
 //we save whatever they input and the location they input it in aka the time block
-function saveItem(taskAndkey) {
-  console.log("This is blue devil! Do you copy HQ?! HQ?! We're in the saveItem() function and need orders!!!");
+function saveItem(x) {
+  //console.log("This is blue devil! Do you copy HQ?! HQ?! We're in the saveItem() function and need orders!!!");
 
-  console.log(taskAndkey);
-  var savedTasks = JSON.parse(localStorage.getItem(taskAndkey.theTime));
+  console.log(x);
+  var savedTasks = JSON.parse(localStorage.getItem(x.theTime));
 
   savedTasks = savedTasks || []; //this is so cool. We asign savedTasks to itself or if savedTasks is empty/null it will asign savedTasks to an empty array. Big fan of this. Quick and short 
-  savedTasks.push(taskAndkey);
+  savedTasks.push(x);
 
-  localStorage.setItem(taskAndkey.theTime, JSON.stringify(savedTasks));
-
+  localStorage.setItem(x.theTime, JSON.stringify(savedTasks));
+  console.log(savedTasks);
 }
 
 function validateData(isTaskempty) {
 
   console.log("inside validateData function");
-  console.log(isTaskempty);
+
+  console.log(isTaskempty.theTask);
 
   if (isTaskempty.theTask === ' ') {
     let truthValue = false;
-    console.log("Task input field empty");
+    //console.log("Task input field empty");
     return truthValue;
   }
 
   else {
     let truthValue = true;
-    console.log("Task input field filled");
-    saveItem(isTaskempty);
+    //console.log("Task input field filled");
     return truthValue;
 
   }
-
+  
 }
 
 function whatTimeisIt() {
 
+  //console.log(taskAndtime);
   let currentHour = dayjs().get('hour');
   //let currentHour = 10;
-  let currentMinute = dayjs().get('minute');
-  console.log(currentHour);
-  console.log(currentMinute);
+  //let currentMinute = dayjs().get('minute');
+  console.log("This is the current hour: " + currentHour);
+  //console.log(currentMinute);
 
 
   $(".container-fluid").children().each(function () {
 
-    let currentDiv = $(".container-fluid").children();
-
+  
     let timeTocompare = $(this).data("time");
-    console.log(timeTocompare);
+    let oneKey = $(this).attr("id")
+    console.log(oneKey);
+    console.log("This is the time to compare: " + timeTocompare);
 
     if (timeTocompare == currentHour) {
 
@@ -75,44 +77,88 @@ function whatTimeisIt() {
       console.log(this);
       $(this).addClass("future")
     }
+
+    renderData(oneKey);
+
   });
 
 }
 
+function renderData(key) {
+  console.log(key);
+  
+  let savedTasks = JSON.parse(localStorage.getItem(key));
+  console.log(savedTasks);
+  console.log(key);
+  let div = document.getElementById(key);
+  console.log(div);
+
+  let textarea = $(div).children('textarea');
+
+  console.log(textarea);
+  if (savedTasks === null || savedTasks.length === 0) {
+    return
+  }
+
+  else {
+    let draw = savedTasks.length -1;
+    let task = savedTasks[draw].theTask;
+    console.log(task);
+    console.log(textarea);
+    let txt = textarea.value = task;
+    document.getElementById(key).querySelector('textarea').innerText = txt;
+  }
+//console.log(taskAndtime.theTask);
+ //localStorage.clear();
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
 
 $(function () {
 
+
   whatTimeisIt();
-
-
+  
 
   //we are pointing at the button using the 'this' keyword. Once we point at the button we specify further with the parent() method. This specifies that we are looking at the parent element  of the 'this' aka the button. We specify further with the attr() which, in this case, grabs the id of that parent element
-  $(".saveBtn").on("click", function () {
-    console.log($(this).parent().attr("id"));
+  $(".saveBtn").on("click", function (taskAndtime) {
 
-
-    let taskInput = $(this).siblings("textarea").val();
-
-    console.log(taskInput);
-    let keyTotheCastle = $(this).parent().attr("id");
-
-    //var taskAndtime = {theTask: task , theTime: key};
+    var keyTotheCastle = $(this).parent().attr("id");
+    var taskInput = $(this).siblings("textarea").val();
     var taskAndtime = {};
+  
     taskAndtime.theTask = taskInput;
     taskAndtime.theTime = keyTotheCastle;
-    console.log(taskAndtime.theTime);
-    console.log(taskAndtime.theTask);
+  
     console.log(taskAndtime);
 
-    validateData(taskAndtime); //this validate that the taskInput is not empty. It still fills the array with a blank taskInput
+    /* let keyTotheCastle = $(this).parent().attr("id");
+    let taskInput = $(this).siblings("textarea").val();
+    var taskAndtime = {};
+  
+    taskAndtime.theTask = taskInput;
+    taskAndtime.theTime = keyTotheCastle;
+    
+    console.log(taskAndtime); */
+    //console.log($(this).parent().attr("id"));
+    //var taskAndtime = {theTask: task , theTime: key};
+    //console.log(taskAndtime.theTime);
+    //console.log(taskAndtime.theTask);
+    //console.log(taskAndtime);
+
+    let x = validateData(taskAndtime); //this validate that the taskInput is not empty. It still fills the array with a blank taskInput
+
+    if (x == true){
+      saveItem(taskAndtime)
+    }
+
+
+
 
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. 
   // HINT: How can the id attribute of each time-block be used to do this?
